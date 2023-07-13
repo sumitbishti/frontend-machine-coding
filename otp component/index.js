320dfc10e98a6a85420a47b7721c7d767c1c724f
sumit <sumit.singh@nineleaps.com>
2023-07-13 22:59:34 +0530

    otp improved

--- otp component/index.js
+++ otp component/index.js
@@ -3,17 +3,30 @@ const inputs = document.querySelectorAll(".input")
 
 inputs[0].focus()
 
+const check = (str) => {
+    return /^[a-zA-Z0-9]$/.test(str);
+}
+
 otpContainer.addEventListener("input", (e) => {
     const target = e.target
     const value = target.value
 
-    if(value === " " || isNaN(value)){
+    if (value === " " || !check(value)) {
         target.value = ""
         return
     }
 
     if (target.nextElementSibling) {
         target.nextElementSibling.focus()
+    }
+})
+
+otpContainer.addEventListener("click", (e) => {
+    for (const input of inputs) {
+        if (input.value == "") {
+            input.focus()
+            break
+        }
     }
 })
 
@@ -34,33 +47,4 @@ otpContainer.addEventListener("keydown", (e) => {
         }
     }
 })
-
-// const form = document.querySelector("#otp-form");
-// const inputs = document.querySelectorAll(".otp-input");
-
-// form.addEventListener("input", (e) => {
-//   const target = e.target;
-//   const value = target.value;
-//   console.log({ target, value });
-//   if (target.nextElementSibling) {
-//     target.nextElementSibling.focus();
-//   }
-// });
-
-// inputs.forEach((input, currentIndex) => {
-//   // backspace event
-//   input.addEventListener("keydown", (e) => {
-//     if (e.keyCode === 8) {
-//       e.preventDefault();
-//       input.value = "";
-//       if (input.previousElementSibling) {
-//         input.previousElementSibling.focus();
-//       }
-//     } else {
-//       if (input.value && input.nextElementSibling) {
-//         input.nextElementSibling.focus();
-//       }
-//     }
-//   });
-// });
 
