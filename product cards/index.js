beb883cb198a0846a89dfd0db2c0cbcef6f04bca
sumit <sumit.singh@nineleaps.com>
2023-07-26 15:56:37 +0530

    changges

--- product cards/index.js
+++ product cards/index.js
@@ -1,46 +1,40 @@
-let data = []
 let container = document.getElementById('container')
-
+let data = []
 
 fetch('https://fakestoreapi.com/products')
     .then(res => res.json())
     .then((res) => {
-        data = res
+        data = res;
+
 
         data.forEach((card) => {
-            let coloredStars = parseInt(card.rating.rate)
-            let uncoloredStars = 5 - coloredStars
+            let filledStars = parseInt(card.rating.rate)
+            let unFilledStars = 5 - filledStars;
+            let starContainer = document.createElement('div')
+            let tmpDiv = document.createElement('div')
+            starContainer.setAttribute("class", "star-container")
 
-            let allStarsDiv = document.createElement('div')
-            allStarsDiv.setAttribute("class", "all_star")
-
-            let allStarsDivContainer = document.createElement('div')
-
-            for (let i = 0; i < coloredStars; i++) {
-                allStarsDiv.innerHTML += `<span class="star-filled">&#9733;</span>`
+            for (let i = 0; i < filledStars; i++) {
+                starContainer.innerHTML += `<span class="star_filled">&#9733;</span>`
             }
-            for (let i = 0; i < uncoloredStars; i++) {
-                allStarsDiv.innerHTML += `<span >&#9734;</span>`
+            for (let i = 0; i < unFilledStars; i++) {
+                starContainer.innerHTML += `<span">&#9734;</span>`
             }
+            
+            tmpDiv.appendChild(starContainer)
 
-            allStarsDivContainer.appendChild(allStarsDiv)
+            container.innerHTML += ` <div class="card">
+            <img class="image" src="${card.image}" alt="image">
+            <div class="card-details">
+                <h3>${card.title}</h3>
+                <p>${card.category}</p>
 
-            container.innerHTML +=
-                `<div class="card">
-                        <img class="image" src=${card.image} alt="product">
-                        <div class="card-details">
-                            <h4>${card.title}</h4>
-                            <p>${card.category}</p>
-                            ${allStarsDivContainer.innerHTML}
-                            <div class="rating-count">
-                                Rating Count : <span>${card.rating.count}</span>
-                            </div>
-                        </div>
-                    <div class="btn">
-                        <button class="buy-btn">
-                            Buy Now
-                        </button>
-                    </div>
-                </div>`
+                ${tmpDiv.innerHTML}
+                <p> Rating Count: <span>${card.rating.count}</span> </p>
+            </div>
+            <button class="buy-btn">Buy Now</button>
+        </div>
+    </div>`
+
         })
     })
