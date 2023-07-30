83911aac78a7f156fe4dd19d15c0918d0580b5bf
Sumit Bisht <sumit7beazt@gmail.com>
2023-07-30 13:20:39 +0530

    tic tac toe

--- tic tac toe/index.js
+++ tic tac toe/index.js
@@ -1,11 +1,11 @@
 let table = document.getElementById('table')
-let tableWrapper = document.getElementById('table-wrapper')
 let resetBtn = document.getElementById('resetBtn')
 let result = document.getElementById('result')
-let hash = {}
-let cross = true;
-let allFilled = 0;
+let cursor = document.getElementById('cursor')
 
+let cross = true;
+let hash = {};
+let allFilled = 0;
 
 for (let i = 0; i < 3; i++) {
     let tr = document.createElement('tr')
@@ -14,118 +14,107 @@ for (let i = 0; i < 3; i++) {
         let td = document.createElement('td')
         td.setAttribute("data-index", `${i}-${j}`)
 
-        if ((i + j) % 2 == 0) {
-            td.setAttribute('class', "red")
-        }
-        else {
-            td.setAttribute('class', "black")
-        }
+        if ((i + j) % 2) td.setAttribute("class", "black")
+        else td.setAttribute("class", "red")
 
         tr.appendChild(td)
     }
     table.appendChild(tr)
 }
 
-const check = () => {
+table.addEventListener("click", (e) => {
+    let indexStr = e.target.dataset.index
+    if (hash[indexStr] || !indexStr) return
 
+    hash[indexStr] = cross ? "X" : 'O';
+    allFilled++;
+
+    e.target.classList.add(cross ? "cross" : "zero")
+    cross = !cross
+
+    let res = checkWin()
+    if (allFilled === 9 || res.includes('Won')) {
+        result.innerHTML = res
+        table.style.pointerEvents = "none"
+        cursor.style.cursor = "not-allowed"
+
+    }
+})
+
+const checkWin = () => {
     //row
     for (let i = 0; i < 3; i++) {
-        let set = new Set()
+        let set = new Set();
         let player = ""
-
         for (let j = 0; j < 3; j++) {
-            let key = `${i}-${j}`
-            player = hash[key]
+            let str = `${i}-${j}`
+            player = hash[str]
             set.add(player)
         }
 
         if (set.size === 1 && player) {
-            return `Player '${player}' has Won!`
+            return `Player ${player} has Won!`
         }
     }
 
-    //col
+    //column
     for (let i = 0; i < 3; i++) {
-        let set = new Set()
+        let set = new Set();
         let player = ""
-
         for (let j = 0; j < 3; j++) {
-            let key = `${j}-${i}`
-            player = hash[key]
+            let str = `${j}-${i}`
+            player = hash[str]
             set.add(player)
         }
 
         if (set.size === 1 && player) {
-            return `Player '${player}' has Won!`
+            return `Player ${player} has Won!`
         }
     }
 
     //diagonal
-    let i = 0;
+    let i = 0, player = "";
     let set = new Set()
-    let player = "";
-
     while (i < 3) {
-        let key = `${i}-${i}`
-        player = hash[key]
+        let str = `${i}-${i}`
+        player = hash[str];
         set.add(player)
         i++;
     }
     if (set.size === 1 && player) {
-        return `Player '${player}' has Won!`
+        return `Player ${player} has Won!`
     }
 
     //anti-diagonal
-    i = 0;
+    i = 0, player = "";
+    set.clear();
     let n = 2;
-    set.clear()
-    player = ""
     while (i < 3) {
-        let key = `${i}-${n - i}`
-        player = hash[key]
+        let str = `${i}-${n - i}`
+        player = hash[str];
         set.add(player)
         i++;
     }
     if (set.size === 1 && player) {
-        return `Player '${player}' has Won!`
+        return `Player ${player} has Won!`
     }
-
-    return "Match Draw!!!"
+    return 'Match Draw!!!'
 }
 
-table.addEventListener("click", (e) => {
-    let str = e.target.dataset.index;
-    if (!str || hash[str]) return;
-
-    hash[str] = cross ? "X" : "O";
-    allFilled++;
-
-    let cellClass = e.target.classList[0]
-    e.target.setAttribute("class", `${cellClass} ${cross ? 'cross' : 'zero'}`)
-    cross = !cross
-
-    let res = check()
-    if (allFilled === 9 || res.includes('Won')) {
-        result.textContent = res
-        table.style.pointerEvents = 'none'
-        tableWrapper.style.cursor = "not-allowed"
-    }
-})
-
-resetBtn.addEventListener("click", (e) => {
-    let crossCells = document.querySelectorAll('.cross')
-    let zeroCells = document.querySelectorAll('.zero')
+resetBtn.addEventListener("click", () => {
+    let crossCells = document.querySelectorAll(".cross")
+    let zeroCells = document.querySelectorAll(".zero")
 
     crossCells.forEach((cell) => {
-        cell.classList.remove('cross')
+        cell.classList.remove("cross")
     })
     zeroCells.forEach((cell) => {
-        cell.classList.remove('zero')
+        cell.classList.remove("zero")
     })
-    cross = true;
+
     hash = {}
+    cross = true
     allFilled = 0
-    result.textContent = ""
     table.style.pointerEvents = "auto"
-    tableWrapper.style.cursor = "pointer"
+    cursor.style.cursor = "pointer"
 })
